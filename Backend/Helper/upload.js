669e9c0eaf5ce1f7ee5cff0f9160d045676ef371
ShweOu-MT <babylay7690love@gmail.com>
2025-06-07 16:16:31 +0630

    first commit

--- Backend/Helper/upload.js
+++ Backend/Helper/upload.js
@@ -1,12 +1,12 @@
 const multer = require('multer');
 
 const storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, __dirname + "/../Public"); // Directory where the files will be stored
-  },
-  filename: function (req, file, cb) {
-    cb(null, Date.now() + '-' + file.originalname); // Filename with a unique suffix
-  }
+  destination:   function (req, file, cb) {
+                cb(null, __dirname + "/../Public"); // Directory where the files will be stored
+                },
+  filename:     function (req, file, cb) {
+                cb(null, Date.now() + '-' + file.originalname); // Filename with a unique suffix
+                }
 });
 
 const upload = multer({ storage: storage });
