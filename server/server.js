39c143cebe7c212918d0940acf84a8e15e0295f4
Min-Thein-void <truek7410@gmail.com>
2025-08-01 12:15:30 +0630

    final bug fixed

--- server/server.js
+++ server/server.js
@@ -10,7 +10,7 @@ app.use(express.static("Public"));
 const cors = require("cors");
 const cookieParser = require("cookie-parser");
 const corsOptions = {
-  origin: "http://localhost:5173",
+  origin: "https://mr-void.vercel.app",
   methods: ["GET", "POST", "PATCH", "DELETE", "OPTIONS"],
   credentials: true,
 };
